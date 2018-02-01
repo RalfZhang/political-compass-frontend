7739c4a224a6930662413eaa090b98abd0f9857a
RalfZhang <zhangkang103@gmail.com>
2018-02-01 23:57:07 +0800

    feature: calculate position value

--- src/containers/Position.jsx
+++ src/containers/Position.jsx
@@ -1,7 +1,30 @@
 import React from 'react';
 
-const Position = () => (
-  <div>abc</div>
-);
+const Position = () => {
+  let answersArr;
+  try {
+    const answersStr = localStorage.getItem('answers');
+    answersArr = answersStr ? JSON.parse(answersStr) : [];
+    if (!answersArr.length) {
+      throw new Error('Length is 0');
+    }
+  } catch (error) {
+    return (
+      <div>Data Error</div>
+    );
+  }
+  const getPositionByReg = (reg) => {
+    const arr = answersArr.filter(e => reg.test(e.id)).map(e => e.value);
+    return arr.reduce((a, c) => a + c) / arr.length;
+  };
+  return (
+    <div>{answersArr.length}
+      <div>{getPositionByReg(/^q1\d\d$/)}</div>
+      <div>{getPositionByReg(/^q2\d\d$/)}</div>
+      <div>{getPositionByReg(/^q3\d\d$/)}</div>
+
+    </div>
+  );
+};
 
 export default Position;
