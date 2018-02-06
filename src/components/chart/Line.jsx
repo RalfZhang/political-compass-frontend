ba1ec2a11d9b316fe67882d0f509e311d6132c64
RalfZhang <zhangkang103@gmail.com>
2018-02-06 23:56:43 +0800

    feature: d3 line chart example

--- src/components/chart/Line.jsx
+++ src/components/chart/Line.jsx
@@ -1,32 +1,38 @@
 import React from 'react';
+import * as d3 from 'd3';
 
-const Bar = () => (
+export default class SvgLine extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: [
+        [0, 80],
+        [100, 100],
+        [200, 30],
+        [300, 50],
+        [400, 40],
+        [500, 80],
+      ],
+    };
+  }
 
-  <svg width="420" height="120">
-    <g transform="translate(0,0)">
-      <rect width="40" height="19" />
-      <text x="37" y="9.5" dy=".35em">4</text>
-    </g>
-    <g transform="translate(0,20)">
-      <rect width="80" height="19" />
-      <text x="77" y="9.5" dy=".35em">8</text>
-    </g>
-    <g transform="translate(0,40)">
-      <rect width="150" height="19" />
-      <text x="147" y="9.5" dy=".35em">15</text>
-    </g>
-    <g transform="translate(0,60)">
-      <rect width="160" height="19" />
-      <text x="157" y="9.5" dy=".35em">16</text>
-    </g>
-    <g transform="translate(0,80)">
-      <rect width="230" height="19" />
-      <text x="227" y="9.5" dy=".35em">23</text>
-    </g>
-    <g transform="translate(0,100)">
-      <rect width="420" height="19" />
-      <text x="417" y="9.5" dy=".35em">42</text>
-    </g>
-  </svg>
-);
-export default Bar;
+  componentDidMount() {
+    const lineGenerator = d3.line();
+
+    const chart = d3.select(this.chart)
+      .attr('width', window.innerWidth - 100)
+      .attr('height', 500)
+      .append('g')
+      .attr('transform', 'translate(100, 0)');
+
+    chart
+      .append('path')
+      .attr('d', lineGenerator(this.state.data));
+  }
+
+  render() {
+    return (
+      <svg className="line-chart--base" ref={r => this.chart = r} />
+    );
+  }
+}
