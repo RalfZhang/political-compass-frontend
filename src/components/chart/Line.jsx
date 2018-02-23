63647706b4103c4e6c8783b3083c7a6a9210fdc6
RalfZhang <zhangkang103@gmail.com>
2018-02-23 23:57:13 +0800

    fix: x axis position

--- src/components/chart/Line.jsx
+++ src/components/chart/Line.jsx
@@ -61,7 +61,7 @@ export default class SvgMultipleLines extends React.Component {
 
     chart.append('g')
       .attr('class', 'axis axis--x')
-      .attr('transform', `translate(0,${y(0) - 20})`)
+      .attr('transform', `translate(0,${y(0) - 0})`)
       .call(d3.axisBottom(x));
 
     chart.append('g')
